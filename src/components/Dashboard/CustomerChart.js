ff03c733ed355ad8e63e6349d6cec9349995d1fc
erickuo5124 <erickuo5124@gmail.com>
2021-10-21 17:58:12 +0800

    feat(CustomerChart): add react echarts

--- src/components/Dashboard/CustomerChart.js
+++ src/components/Dashboard/CustomerChart.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import { Flex } from '@chakra-ui/layout'
+import ReactECharts from 'echarts-for-react';
 
 export default function CustomerChart() {
+  const chartOption = {
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        data: [150, 230, 224, 218, 135, 147, 260],
+        type: 'line'
+      }
+    ],
+    tooltip: {
+      trigger: 'axis',
+    }
+  };
   return (
-    <div>
-      CustomerChart
-    </div>
+    <Flex
+      w='50%'
+      direction='column'
+      bgColor='white'
+      border='solid 1px #e3e3e3'
+      borderRadius={3}
+    >
+      <Flex bgColor='#fafafa' p={3} fontWeight='semibold'>
+        Key Number
+      </Flex>
+      <ReactECharts style={{height: '100%'}} option={chartOption} />
+    </Flex>
   )
 }
